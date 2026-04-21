8a20fced777486eec9f160208470d69be061281d
liyanrong <1778449029@qq.com>
2026-04-21 23:28:16 +0800

    finish

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,10 +35,22 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let p: Vec<_> = s.split(',').collect();
+        if p.len() != 2 {
+            return Person::default();
+        }
+        let name = p[0];
+        let age = p[1].parse::<usize>();
+        if age.is_err() || name.is_empty() {
+            return Person::default();
+        }
+        Person {
+            name: p[0].to_string(),
+            age: p[1].parse::<usize>().unwrap_or(30),
+        }
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -28,7 +28,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -46,6 +45,31 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.is_empty() {
+            return Err(ParsePersonError::Empty);
+        }
+        let idx = s.find(',');
+        if let Some(idx) = idx {
+            if idx == 0 {
+                return Err(ParsePersonError::NoName);
+            } else {
+                let strs: Vec<_> = s.split(',').collect();
+                if strs.len() != 2 {
+                    return Err(ParsePersonError::BadLen);
+                }
+                let age = strs[1].parse::<usize>();
+                if age.is_ok() {
+                    return Ok(Person {
+                        name: strs[0].to_string(),
+                        age: age.unwrap(),
+                    });
+                } else {
+                    return Err(ParsePersonError::ParseInt(age.unwrap_err()));
+                }
+            }
+        } else {
+            return Err(ParsePersonError::BadLen);
+        }
     }
 }
 
